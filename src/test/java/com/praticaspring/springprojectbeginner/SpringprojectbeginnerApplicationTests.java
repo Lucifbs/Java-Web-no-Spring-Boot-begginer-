d2064ef1c618401a0f21089ec4bf4ea6cbda5d26
L Fernanda <brasillucia@gmail.com>
2023-03-24 14:55:11 -1000

    primeiro commit

---
+++ src/test/java/com/praticaspring/springprojectbeginner/SpringprojectbeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praticaspring.springprojectbeginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojectbeginnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
